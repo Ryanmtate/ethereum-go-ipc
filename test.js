bd25955eec5cef320b74dae41a41984be738ab8b
Ryan Tate <ryan@ventureequity.co>
2016-04-22 16:44:01 -0700

    Removing promisification of web3

--- test.js
+++ test.js
@@ -6,6 +6,8 @@ geth.setGethSocket("/Users/ryan/Library/Ethereum/geth.ipc");
 geth.delay(3000).then(() => {
   return geth.newAccount('password');
 }).then((created) => {
+  return geth.newAccount('password');
+}).then((created) => {
   return geth.listAccounts();
 }).then((accounts) => {
   console.log(accounts);
